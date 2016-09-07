e7e3769c65e931319198d5fd74d82238871294fa
Rafael Gonzalez <astrorafael@yahoo.es>
2016-09-07 18:18:21 +0200

    changed separator characterfrom , to ;

--- roles/nto.reports/templates/tess_bulk_dump_by_instrument.sh
+++ roles/nto.reports/templates/tess_bulk_dump_by_instrument.sh
@@ -4,6 +4,7 @@
 instrument_name=$1
 
 sqlite3 -csv -header /var/dbase/tess.db <<EOF
+.separator ;
 SELECT (d.julian_day + t.day_fraction) AS julian_day, (d.sql_date || 'T' || t.time || 'Z') AS timestamp, r.sequence_number, l.site, i.name, r.frequency, r.magnitude, i.calibration_k, r.sky_temperature, r.ambient_temperature
 FROM tess_readings_t AS r
 JOIN tess_t          AS i USING (tess_id)
