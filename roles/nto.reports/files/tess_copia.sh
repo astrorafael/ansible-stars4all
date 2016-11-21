660be0a5f2f37904b73bbf0519cd94775a40bb36
Rafael Gonzalez <astrorafael@yahoo.es>
2016-11-21 14:50:16 +0100

    updated remote copy script

--- roles/nto.reports/files/tess_copia.sh
+++ roles/nto.reports/files/tess_copia.sh
@@ -6,6 +6,7 @@ HOST=rb-tess.fis.ucm.es
 SRCDIR=/var/dbase/reports
 DATADIR=$HOME/tess_data
 SUMMARY_FILE=tess_readings_daily_summary.txt
+CATALOG_FILES=tess_c*.csv
 DATA_FILES=*.csv.gz
 
 
@@ -14,6 +15,7 @@ DATA_FILES=*.csv.gz
 
 # REALIZA LA COPIA
 echo "Copiando datos exportados de TESSSD a ${DATADIR}"
-scp ${USER}@${HOST}:${SRCDIR}/${SUMMARY_FILE} ${DATADIR}
-scp ${USER}@${HOST}:${SRCDIR}/${DATA_FILES} ${DATADIR}
+scp ${USER}@${HOST}:${SRCDIR}/${SUMMARY_FILE}  ${DATADIR}
+scp ${USER}@${HOST}:${SRCDIR}/${CATALOG_FILES} ${DATADIR}
+scp ${USER}@${HOST}:${SRCDIR}/${DATA_FILES}    ${DATADIR}
 echo "Hecho"
